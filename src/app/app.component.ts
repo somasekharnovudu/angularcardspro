e3f9a446032ee77de3567911db44a18a82aabc1d
Soma Sekhar Novudu <novudu.somasekhar702@gmail.com>
2017-12-29 12:06:54 +0530

    improved cart functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -18,15 +18,33 @@ export class AppComponent implements OnInit {
   }
   cartTotal($event){
       let catcher=0;
-    for(let item of this.cartitems){
-      if(item.title==$event){
-        item.quantity += 1;
-        catcher=1;
+      if(this.cartitems.length>0){
+        for(let item of this.cartitems){
+          if(item.title==$event){
+            item.quantity += 1;
+            catcher=1;
+            break;
+          }else{
+            catcher=0;
+          }
+        }
+      }
+      if(catcher==0){
+        this.cartitems.push({title:$event,quantity:1})
+      }
+    console.log(this.cartitems);
+    console.log(this.cards);
+  }
+  cartremoval($event){
+    for(let i in this.cartitems){
+      console.log(i);
+      if(this.cartitems[i].title==$event){
+        if(this.cartitems[i].quantity>1){
+            this.cartitems[i].quantity--;
+        }else{
+           this.cartitems.splice(+i,1);
+        }
       }
     }
-    if(!catcher){
-      this.cartitems.push({title:$event,quantity:1})
-    }
-    console.log(this.cartitems);
   }
 }
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -8,11 +8,35 @@ import {CardService} from './card.service';
 })
 export class CardComponent implements OnInit{
   @Input('card') card;
+  @Input('cart') cart;
   @Input ("desclimit") desclimit:string; 
   @Output('addCart') addCart=new EventEmitter();
+  @Output('removeCart') removeCart=new EventEmitter();
   constructor(){}
   ngOnInit(){}
   cartadding(value){
     this.addCart.emit(value);
+    this.detectChange()
+  }
+  cartremoving(value){
+    this.removeCart.emit(value);
+    this.detectChange();
+  }
+  detectChange(){
+    let catcher=0;
+    if(this.cart.length){
+      for(var i in this.cart){
+        if(this.cart[i].title==this.card.title){
+          this.card["quantity"]=this.cart[i].quantity;
+          catcher=1;
+          break;
+        }else{
+          catcher=0;
+        }
+      }
+    }
+    if(catcher==0){
+      this.card.quantity=0;
+    }
   }
 }
